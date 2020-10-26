c17fceed32a824d869cc10b6c87fa913b231e8ed
UPiracy <upiracy@163.com>
2020-10-26 11:44:52 +0800

    修改了SSAP，增加了PostProcessing控件

--- Assets/Scripts/Effects/PostProcessing.cs
+++ Assets/Scripts/Effects/PostProcessing.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class PostProcessing : MonoBehaviour
 {
     [SerializeField] VolumeProfile profile;
+    Vignette vignette;
+    SplitToning toning;
+    WhiteBalance balance;
+    ColorAdjustments adjustment;
 
-    void Start()
+    static PostProcessing post;
+
+    private void Start()
     {
-        
+        post = this;
+        profile.TryGet<Vignette>(out vignette);
+        profile.TryGet<SplitToning>(out toning);
+        profile.TryGet<WhiteBalance>(out balance);
+        profile.TryGet<ColorAdjustments>(out adjustment);
     }
 
-    // Update is called once per frame
-    void Update()
+    public static void SetVignette(float smoothness)
     {
-        
+        post.vignette.smoothness = new ClampedFloatParameter(smoothness, 0, 1);
+    }
+
+    public static void SetTemperature(float temperature)
+    {
+        post.balance.temperature = new ClampedFloatParameter(temperature, -100, 100);
+    }
+
+    public static void SetContrast(float contrast)
+    {
+        post.adjustment.contrast = new ClampedFloatParameter(contrast, -100, 100);
+    }
+
+    public static void SetSaturation(float saturation)
+    {
+        post.adjustment.saturation = new ClampedFloatParameter(saturation, -100, 100);
     }
 }
